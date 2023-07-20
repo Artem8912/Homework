9c6ace63006def2022138245738eb654c792c6eb
Art <artemgoryunov89@gmail.com>
2023-07-20 17:35:39 +0400

    Написали программу по определению max числа из трёх(1*)

---
+++ max-of-three/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+if (a > c)
+{
+Console.WriteLine($"{a} - максимальное число ");
+}
+}
+else
+{
+if (b > c)
+{
+Console.WriteLine($"{b} - максимальное число ");
+}
+else
+{
+Console.WriteLine($"{c} - максимальное число ");
+}
+}
