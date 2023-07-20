b228117b240e9ee1e808a35d12c7182fa868bc5e
Art <artemgoryunov89@gmail.com>
2023-07-20 18:05:35 +0400

    Программа по определению чётности и нечётности числа

---
+++ Even-number/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($"{a} - чётное число");
+}
+else
+{
+    Console.WriteLine($"{a} - нечётное число");
+
+}
