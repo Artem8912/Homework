f1cbc8201037e7633ed65c99c413f440a814ae36
Art <artemgoryunov89@gmail.com>
2023-07-20 16:48:26 +0400

    написали программу на max и min число

---
+++ Max-of-two/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = ConvertToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int b = ConvertToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+
+if (a >b) 
+{
+    Console.Write($"{a} - большее число");
+    Console.Write($"{b} - меньшее число");  
+    
+}
+
+else 
+{
+   Console.Write($"{b} - большее число");
+   Console.Write($"{a} - меньшее число"); 
+}
